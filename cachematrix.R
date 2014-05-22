eb4194c4cc394618755f20298cbd51e7b7e812cf
Rodney Purkey <rodney.purkey@gmail.com>
2014-05-22 17:33:53 -0500

    Inverse matrix functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This pair of functions will calculate and return the inverse
+## of an invertible matrix. As matrix inversion is a costly operation, 
+## this pair of functions takes advantage of caching results for 
+## subsequent retrieval.
 
-## Write a short comment describing this function
+## makeCacheMatrix() takes a square invertible matrix as it's only argument.
+## The <<- operator is used to assign a value to an object in an environment
+## that is different from the current environment.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) m <<- solve
+  getsolve <- function() m
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
-
-## Write a short comment describing this function
+## cacheSolve() takes as it's argument a list object created by calling makeCacheMatrix().
+## The function calculates the inverse of the matrix created in makeCacheMatrix().
+## The member function getsolve() is called to check if the matrix inverse has already 
+## been calculated and retrieves the result.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getsolve()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data)
+  x$setsolve(m)
+  m
 }
